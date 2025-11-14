5fdb766eba6ec4f10dd294eafdaf144456116b9c
bharath <bairyjbs9@gmail.com>
2025-11-14 15:20:26 +0530

    pushed new changes

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-console.log("hello") 
-console.log("hello") 
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
